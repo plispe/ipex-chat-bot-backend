38b3b5e742eda1d60d362184f495135f97c0aab0
Petr Pliska <petr.pliska@post.cz>
2019-05-29 13:58:17 +0200

    Add basic logic

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
 const express = require('express')
+const {Wit, log} = require('node-wit')
+
+const pages = {
+    'produktovy_katalog': 'https://ipexas.atlassian.net/wiki/spaces/IPK/overview',
+    'ceniky-mobily': 'https://ipexas.atlassian.net/wiki/spaces/IPK/pages/763559968/Mobily',
+    'produktovy_katalog-velkoobchod': 'https://ipexas.atlassian.net/wiki/spaces/IPK/pages/756350977/Produkty+pro+velkoobchodn+partnery+ISP+TSP',
+    'produktovy_katalog-maloobchod': 'https://ipexas.atlassian.net/wiki/spaces/IPK/pages/743243780/Produkty+pro+firmy',
+    'produktovy_katalog-mobily': 'https://ipexas.atlassian.net/wiki/spaces/IPK/pages/763559968/Mobily',
+}
+
+const client = new Wit({
+    accessToken: process.env.WIT_AI_API_TOKEN,
+    logger: new log.Logger(log.DEBUG) // optional
+})
 
 const app = express()
 app.use(express.json())
 
 const {PORT: port = 3000} = process.env
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     const {msg, id} = req.body
+    const {entities} = await client.message(msg)
+    console.log(entities)
 
-    switch (msg.toLowerCase()) {
-        case 'ping': 
-          res.json({msg: 'pong'})
-          break
+    let {intent=[], pricelist_type = []} = entities
+    let replyText = null
+
+    if (intent.length) {
+        intent = intent[0].value
+    }
+    
+    switch (intent) {
+        case 'produktovy_katalog':
+            replyText = `Zde je odkaz na Nás produktový katalog: ${pages[intent]}.`
+        case 'ceniky':
+            if (pricelist_type.length) {
+                const {value: pricelistType = null} = pricelist_type[0]
+                replyText = `Zde je odkaz na cenik pro ${pricelistType}: `
+            }
+            break;
         default:
-          res.json({msg})
+            replyText = 'Nerozumím otázce, zkuste to prosím znovu. Děkuji.'
+    }  
+
+    if (replyText) {
+        res.json({msg: replyText})
     }
 })
 
